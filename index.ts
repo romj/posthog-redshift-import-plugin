8b3d0fb179795f3692c449e893a03c53576dbe97
maelstonly <92147460+maelstonly@users.noreply.github.com>
2021-10-14 09:43:21 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ interface TransformationsMap {
     }
 }
 const EVENTS_PER_BATCH = 10
-const REDIS_OFFSET_KEY = dze
+const REDIS_OFFSET_KEY = 'dzezef'
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
     return unquotedIdentifier
 }
