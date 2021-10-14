88b9bc8ba6b2f2c27e92b44448b95d6694a5d445
maelstonly <92147460+maelstonly@users.noreply.github.com>
2021-10-14 08:30:12 +0200

    dezzzrg

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ interface TransformationsMap {
     }
 }
 const EVENTS_PER_BATCH = 10
-const REDIS_OFFSET_KEY = 'import_offset_dze'
+const REDIS_OFFSET_KEY = 'import_offset_dzdee'
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
     return unquotedIdentifier
 }
