fc7e9183cf0f7f66d79e4864567c0fc9e9b37d82
mael guerin <mael.guerin@stonly.com>
2021-10-15 10:52:29 +0200

    greegrrgegre

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ interface TransformationsMap {
     }
 }
 const EVENTS_PER_BATCH = 10
-const IS_CURRENTLY_IMPORTING = 'redshift_importing'
+const IS_CURRENTLY_IMPORTING = 'redshift_importingb'
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
     return unquotedIdentifier
 }
